03668547deb4d238252380dd64ea0a912aa6fff8
LouisHyt <louishayotte@gmail.com>
2023-06-06 16:45:27 +0200

    Ajout de l'exercice 10

--- Partie 2/exo10.php
+++ Partie 2/exo10.php
@@ -7,5 +7,90 @@
 
 <?php
 
+
+    $genre = ["Homme","Femme","Autre"];
+    $formInput = ["Nom", "Prénom", "Adresse", "e-mail"];
+    $formationOptions = ["Développeur Logiciel", "Designer web", "Intégrateur", "Chef de projet"];
+    $genreOptions = [
+        "Homme" => false,
+        "Femme" => true,
+        "Autre" => false
+      ];
+
+      function generateGenreInput($genreOptions){
+        $result = "<div class='genre'>";
+        foreach ($genreOptions as $option => $isChecked) {
+            $radioName = str_replace(" ", "", strtolower($option));
+            $checkedValue = $isChecked ? "checked" : "unchecked";
+            $result .= "
+            <div>
+                <input type='radio' name='radios' $checkedValue/>
+                <label for='$radioName'>$option</label>
+            </div>
+            ";
+        }
+        $result .= '</div>';
+        return $result;
+      }
+
+    function generateSubmitButton($text){
+        $button = "<input type='submit' name='submit' value='$text'>";
+        return $button;
+    }
+    
+    function generateInfos($requiredInfos){
+        foreach ($requiredInfos as $field) {
+            $lcField = strtolower($field);
+            echo("
+                <div>
+                    <label for='lcField'>$field</label>
+                    <input type='text' required name='$lcField' class='fieldinput'/>
+                </div>
+            ");
+        }
+    }
+
+    function generateFormation($formationOptions){
+        $options = "<label for='selection' >Intitulé de formation</label><select name='selection'>";
+        foreach($formationOptions as $option){
+            $options .= "<option value='".strtolower($option)."'>$option</option>";
+        }
+
+        $options .= "</select>";
+        
+        return $options;
+    }
+
+
+    echo("<form method='POST' action='/getFormData.php'>");
+    echo(generateInfos($formInput));
+    echo(generateGenreInput($genreOptions));
+    echo(generateFormation($formationOptions));
+    echo("<br />");
+    echo(generateSubmitButton("Envoyer le formulaire"));
+    echo("</form>");
+    
     
 ?>
+
+
+<style>
+    div > * {
+        display: block;
+    }
+
+    div{
+        margin-block: 5px;
+    }
+    
+    .genre{
+        display: flex;
+        gap: 15px;
+    }
+
+    input[type="submit"]{
+        margin-top: 10px;
+    }
+
+
+</style>
